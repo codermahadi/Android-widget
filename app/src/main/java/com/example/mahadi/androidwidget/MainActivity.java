48fb5a6fa184538f8ccedd1343ddaa8bbbe24df1
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-01-23 23:58:42 +0600

    using setText method for set text into TextView

--- app/src/main/java/com/example/mahadi/androidwidget/MainActivity.java
+++ app/src/main/java/com/example/mahadi/androidwidget/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mahadi.androidwidget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView name,age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.name);
+        name.setText("Anisul Islam");
+
+
+        age = (TextView) findViewById(R.id.age);
+
+        age.setText("25 years Old");
     }
 }
