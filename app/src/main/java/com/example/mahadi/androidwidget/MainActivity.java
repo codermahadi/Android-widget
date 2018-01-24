89b30c3b35736b459b793c977ccdb68c9e198d45
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-01-24 23:12:20 +0600

    Add button and setOnClickListener

--- app/src/main/java/com/example/mahadi/androidwidget/MainActivity.java
+++ app/src/main/java/com/example/mahadi/androidwidget/MainActivity.java
@@ -2,22 +2,37 @@ package com.example.mahadi.androidwidget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView name,age;
+    private TextView name,age,textShow;
+    Button btn;
+    public int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         name = (TextView) findViewById(R.id.name);
-        name.setText("Anisul Islam");
-
-
+        textShow = (TextView) findViewById(R.id.text3);
         age = (TextView) findViewById(R.id.age);
 
+        name.setText("Anisul Islam");
         age.setText("25 years Old");
+
+        btn  = (Button) findViewById(R.id.btn);
+        
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                textShow.setText("You clicked " + count + " Times");
+            }
+        });
+
     }
 }
